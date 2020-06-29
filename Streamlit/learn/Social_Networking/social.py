6e991c285ba93d637e35fa9eb1c89f7063aba5c4
RishitOMG <yogamer0101@gmail.com>
2020-06-29 11:58:55 +0530

    TEST:- Latex converter

--- Streamlit/learn/Social_Networking/social.py
+++ Streamlit/learn/Social_Networking/social.py
@@ -1,7 +1,16 @@
 import streamlit as st
 
+# Title
 st.latex("\LARGE \LaTeX {Calculator}")
 st.title("")
 r'''
 $$\text{Heard o'} \space \LaTeX \space \text{lets make a calculator featuring it!}$$
 '''
+
+# Sample stuff remove # to use
+# Converst LaTeX code into output
+
+# input = st.text_input("Type in LaTeX code :- ")
+
+# if st.button("Convert!"):
+#    st.latex(input)
