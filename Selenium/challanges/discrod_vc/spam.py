7f96c536016ac450dd3a74c509274445f94128cd
RishitOMG <yogamer0101@gmail.com>
2020-07-15 11:44:27 +0530

    commiting my changes

--- Selenium/challanges/discrod_vc/spam.py
+++ Selenium/challanges/discrod_vc/spam.py
@@ -9,7 +9,7 @@ browser.get("https://discord.com/login")
 login = browser.find_element_by_css_selector("input[name='email']")
 login.send_keys(input("Type in an email:- "))
 password = browser.find_element_by_css_selector("input[name='password']")
-password.send_keys(getpass(prompt="Type in your password"))
+password.send_keys(getpass(prompt="Type in your password:- "))
 login_button = browser.find_element_by_css_selector("button[type='submit']")
 login_button.click()
 
@@ -18,7 +18,7 @@ server = "a[aria-label='Tech World']"
 server_elem = browser.find_element_by_css_selector(server)
 server_elem.click()
 
-vc = "//div[text()='General vc']"
+vc = "//div[@class='content-3at_AU']/div[text()='General vc']"
 vc_elem = browser.find_element_by_xpath(vc)
 
 vc_mute = "//button[@aria-label='Deafen']"
@@ -26,5 +26,7 @@ vc_mute_elem = browser.find_element_by_xpath(vc_mute)
 
 vc_elem.click()
 # vc_mute_elem.click()
-for i in range(0,50):
+for i in range(0,10):
     vc_mute_elem.click()
+
+browser.quit()
