bc49d7a87b74fa2df85d789aeff4864447f537fc
RishitOMG <yogamer0101@gmail.com>
2020-07-24 13:36:57 +0530

    Describing Data (PANDAZZZZZZZZZZZZZZZZZ!)

--- Pandas/learn/Basics/basics.py
+++ Pandas/learn/Basics/basics.py
@@ -29,3 +29,38 @@ car_sales
 
 car_sales.to_csv("exported-car-sales.csv", index=False) # If you dont put "index=False" it will export with the index values
 # CSV is a univeral datatype
+
+
+# Describe Data in Pandas
+
+# Attributes
+# Describing DataTypes
+car_sales.dtypes # Its an attribute as it dosent have "()" after it # A Function does have () after it
+
+# Tells us our column names
+car_sales.columns
+
+# Tells us about the index (range of it)
+car_sales.index
+
+# Functions
+
+# Gives us some statical info about our numeric columns
+car_sales.describe()
+
+# Gives us all sorts of information
+car_sales.info()
+
+# Gives the average of the numeracial columns
+car_sales.mean()
+# You can even call it on individual series
+car_prices = pd.Series([3000, 1500, 111250])
+car_prices.mean()
+
+car_sales.sum()
+
+# Selecting a single column
+car_sales["Doors"].sum()
+
+# Tells the number of rows
+len(car_sales)
