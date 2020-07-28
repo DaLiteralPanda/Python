cd4422df099afe27ba045bdca739680a9c8de5dc
RishitOMG <yogamer0101@gmail.com>
2020-07-28 09:07:42 +0530

    making my edits

--- Matplotlib/basics/intro.py
+++ Matplotlib/basics/intro.py
@@ -32,4 +32,28 @@ plt.show()
 # 3rd Method (recommended)
 fig, ax = plt.subplots()
 ax.plot(x, [50, 100, 200, 250]); # Adds some data
-|
+type(fig), type(ax)
+
+#####
+# Matplotlib example workflow
+#####
+
+# 0. Import matplotlib
+%matplotlib inline
+import matplotlib.pyplot as plt
+
+# 1. Prepare data
+x = [1, 2, 3, 4]
+y = [11, 12, 33, 45]
+
+# 2. Setup Plot
+fig, ax = plt.subplots(figsize=(5, 5)) # width, height
+
+# 3. Plot the data
+ax.plot(x, y)
+
+# 4. Customise plot
+ax.set(title="Simple Plot", xlabel="x-axis", ylabel="y-axis")
+
+# 5. Save & show (the whole figure)
+fig.savefig("./img/sample-plot.png")
