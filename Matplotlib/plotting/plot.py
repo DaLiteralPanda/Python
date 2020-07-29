40c975cd12916ded058688983aac65a0c988d4a2
RishitOMG <yogamer0101@gmail.com>
2020-07-29 19:51:00 +0530

    making my edits

--- Matplotlib/plotting/plot.py
+++ Matplotlib/plotting/plot.py
@@ -59,3 +59,9 @@ ax3.bar(nut_butter.keys(), nut_butter.values());
 ax4.hist(np.random.randn(100));
 
 # Option 2
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 5))
+# Plot to each different index
+ax[0, 0].plot(x, x/2);
+ax[0, 1].scatter(np.random.random(10), np.random.random(10));
+ax[1, 0].bar(nut_butter.keys(), nut_butter.values());
+ax[1, 1].hist(np.random.randn(100));
