cb8644be6640aa2dab71c087c67b6484b7745afa
DaLiteralPanda <yogamer0101@gmail.com>
2020-09-21 00:17:57 +0530

    map()

--- Rishov/Advanced_Python_Meetup/lambda_with_comprehensions.py
+++ Rishov/Advanced_Python_Meetup/lambda_with_comprehensions.py
@@ -4,3 +4,27 @@
 ## Anonymous functions in Python
 ## Clean and consise code
 ## Pairs well with iterable methods
+
+def fname2(x):
+    return 2*x
+
+fname = lambda x: 2*x
+
+fname2(4)
+
+# List with numbers 1 to 100
+
+my_list = [i for i in range(1, 101)]
+
+# Example of applying function to elements in list comprehensions
+f_list = [fname(i) for i in my_list]
+
+# Using map
+
+# Create the above list using map and lambda
+f_list_2 = map(lambda x:2*x, my_list)
+f_list_2
+
+list(f_list_2)
+
+# map(function exprension, )
